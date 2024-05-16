91666d955fab22d19aea8ddac16d81758ad1c9ea
Pirskiy Maxim <pirskiymi98@gmail.com>
2024-05-16 17:41:38 +0300

    Updates

---
+++ assets/index-DQ8pYHz1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DZQr70gZ.js";import{F as e}from"./Fallback-B8QRaqRE.js";const i=()=>t.jsx("div",{style:{height:"100%",display:"flex",alignItems:"center",justifyContent:"center"},children:t.jsx(e,{})});export{i as ErrorPage};
