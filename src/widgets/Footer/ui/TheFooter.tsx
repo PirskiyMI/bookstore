1c08ea2f88142b1734693fdd90f879fa72f5c4ee
PirskiyMI <pirskiymi98@gmail.com>
2024-05-17 23:27:46 +0300

    fix theFooterColumn

--- src/widgets/Footer/ui/TheFooter.tsx
+++ src/widgets/Footer/ui/TheFooter.tsx
@@ -3,15 +3,15 @@ import { FC } from 'react';
 import { popularCategoryLinks } from '../constants/popularCategoryLinks';
 import { usefulLinks } from '../constants/usefulLinks';
 
-import { TheFooterColumns } from './TheFooterColumns';
+import { TheFooterColumn } from './TheFooterColumn';
 import styles from './TheFooter.module.scss';
 
 export const TheFooter: FC = () => {
    return (
       <div className={styles.footer}>
          <div className={`${styles.footer__container} container`}>
-            <TheFooterColumns title="Popular books" list={popularCategoryLinks} />
-            <TheFooterColumns title="Useful links" list={usefulLinks} />
+            <TheFooterColumn title="Popular books" list={popularCategoryLinks} />
+            <TheFooterColumn title="Useful links" list={usefulLinks} />
          </div>
       </div>
    );
--- src/widgets/Footer/ui/TheFooterColumn.tsx
+++ src/widgets/Footer/ui/TheFooterColumn.tsx
@@ -1,19 +1,20 @@
 import { FC } from 'react';
-import style from './TheFooterColumns.module.scss';
 import { NavLink } from 'react-router-dom';
+
+import style from './TheFooterColumns.module.scss';
 
 interface IProps {
    list: { title: string; path: string; isLink: boolean }[];
    title: string;
 }
 
-export const TheFooterColumns: FC<IProps> = ({ list, title }) => {
+export const TheFooterColumn: FC<IProps> = ({ list, title }) => {
    return (
-      <div className={style.footerColumns}>
-         <h5 className={style.footerColumns__title}>{title}</h5>
-         <ul className={style.footerColumns__list}>
+      <div className={style.footerColumn}>
+         <h3 className={style.footerColumn__title}>{title}</h3>
+         <ul className={style.footerColumn__list}>
             {list.map(({ path, title, isLink }) => (
-               <li key={title} className={style.footerColumns__item}>
+               <li key={title} className={style.footerColumn__item}>
                   {isLink ? (
                      <NavLink to={path}>{title}</NavLink>
                   ) : (
