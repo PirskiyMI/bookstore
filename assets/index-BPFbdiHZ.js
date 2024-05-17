62cec7fb405b89aa2e455034d2cacb8cac707d0e
PirskiyMI <pirskiymi98@gmail.com>
2024-05-17 23:28:14 +0300

    Updates

--- assets/index-BPFbdiHZ.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Dxn5e6RA.js";import{F as e}from"./Fallback-B4qEx2kN.js";const i=()=>t.jsx("div",{style:{height:"100%",display:"flex",alignItems:"center",justifyContent:"center"},children:t.jsx(e,{})});export{i as ErrorPage};
---
+++ assets/index-CoSdHXJm.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CO28X3LS.js";import{F as e}from"./Fallback-CSlZkx1i.js";const i=()=>t.jsx("div",{style:{height:"100%",display:"flex",alignItems:"center",justifyContent:"center"},children:t.jsx(e,{})});export{i as ErrorPage};
