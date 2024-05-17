71d9207d1647dae753699e6ea3b9e8e02b014ef1
PirskiyMI <pirskiymi98@gmail.com>
2024-05-17 10:46:38 +0300

    refactoring

--- src/widgets/Banner/ui/Banner.tsx
+++ src/widgets/Banner/ui/Banner.tsx
@@ -1,34 +1,45 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
-import { Link } from 'react-router-dom';
+import { SwiperOptions } from 'swiper/types';
 
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 
-import { slides } from '../constants';
-
-import styles from './Banner.module.scss';
 import { SliderController } from 'shared/ui/SliderController';
 
+import { slides } from '../constants/slides';
+
+import styles from './Banner.module.scss';
+
 export const Banner: FC = () => {
+   const swiperConfigs: SwiperOptions = useMemo(
+      () => ({
+         modules: [Navigation, Pagination, Autoplay],
+         autoplay: { delay: 4000 },
+         navigation: false,
+         pagination: { clickable: true },
+      }),
+      [],
+   );
+   const slideList: JSX.Element = (
+      <>
+         {slides.map(({ imgURL, ISBN13, title }, index) => (
+            <SwiperSlide key={index} className={styles.banner__item}>
+               <Link to={`/books/${ISBN13}`} className={styles.banner__link} state={title}>
+                  <img src={imgURL} alt="" className={styles.banner__img} />
+               </Link>
+            </SwiperSlide>
+         ))}
+      </>
+   );
+
    return (
       <div className={styles.banner}>
-         <Swiper
-            modules={[Navigation, Pagination, Autoplay]}
-            autoplay={{ delay: 4000 }}
-            slidesPerView={1}
-            navigation={false}
-            pagination={{ clickable: true }}>
-            {slides.map(({ imgURL, ISBN13, title }, index) => (
-               <SwiperSlide key={index} className={styles.banner__item}>
-                  <Link to={`/books/${ISBN13}`} className={styles.banner__link} state={title}>
-                     <img src={imgURL} alt="" className={styles.banner__img} />
-                  </Link>
-               </SwiperSlide>
-            ))}
-
+         <Swiper {...swiperConfigs}>
+            {slideList}
             <SliderController />
          </Swiper>
       </div>
--- src/widgets/Footer/ui/TheFooterColumns.tsx
+++ src/widgets/Footer/ui/TheFooterColumns.tsx
@@ -3,24 +3,24 @@ import style from './TheFooterColumns.module.scss';
 import { NavLink } from 'react-router-dom';
 
 interface IProps {
-   list: { title: string; path: string; isLink: boolean; }[];
+   list: { title: string; path: string; isLink: boolean }[];
    title: string;
 }
 
-export const TheFooterColumns: FC<IProps> = ({ list, title, }) => {
+export const TheFooterColumns: FC<IProps> = ({ list, title }) => {
    return (
       <div className={style.footerColumns}>
-         <h5 className={`${style.footerColumns__title} title`}>{title}</h5>
+         <h5 className={style.footerColumns__title}>{title}</h5>
          <ul className={style.footerColumns__list}>
             {list.map(({ path, title, isLink }) => (
                <li key={title} className={style.footerColumns__item}>
-                  {
-                     isLink ? (
-                        <NavLink to={path}>{title}</NavLink>
-                     ) : (
-                        <a href={path} target="_blank">{title}</a>
-                     )
-                  }
+                  {isLink ? (
+                     <NavLink to={path}>{title}</NavLink>
+                  ) : (
+                     <a href={path} target="_blank">
+                        {title}
+                     </a>
+                  )}
                </li>
             ))}
          </ul>
